818056b18115bfa70e5cbedfac59bbe4fa70a7d4
isRifat29 <dev.rifat29@gmail.com>
2025-05-27 13:43:03 +0600

    initial commit

---
+++ Main.py
@@ -0,0 +1,16 @@
+#exercise 1
+#take name as input and print a greeting
+# user_name = input("Enter your user name: ")
+# print("Hello, "+user_name)
+
+#exercise 2
+#take two number as input and print the sum
+# first_number = int(input("Enter the first number: "))
+# second_number = int(input("Enter the second number: "))
+# print("Sum of two number: ",(first_number+second_number))
+
+#exercise 3
+#calculate the area of a rectangle
+width = float(input("Enter the rectangle width: "))
+height = float(input("Enter the rectangle height: "))
+print("The are of rectangle: ",(width * height))
